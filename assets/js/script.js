ca0cc32a0dabdc3b2009dbe66822133c74e467b1
jennyk1989 <jennifer0keene@gmail.com>
2021-12-13 12:54:19 -0600

    removed key

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,19 +20,7 @@ fetch(testURL)
 function getOpenWeather () {
     let weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-}
 
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Basic ZTdlNjRlNGE4MjUxZjI3OWMyZmU2NWZlYjZkYTJhNTI6YmM4Y2E5ODBkMDliN2Y3ZjgwOTc5MjgzOTAyNzFkYzk=");
-
-var raw = "";
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
 
 let newURL = "https://api.roadgoat.com/api/v2/destinations/auto_complete?q=chicago"
 fetch(newURL, requestOptions)
