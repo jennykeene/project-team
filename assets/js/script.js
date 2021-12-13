205d380177782f35c64763a94afb0cc7c3ee7bd5
jennyk1989 <jennifer0keene@gmail.com>
2021-12-13 13:02:27 -0600

    api security files added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //VARIABLES:
-
+var token = config.MY_API_TOKEN;
+var key = config.SECRET_API_KEY;
 
 //Covid Cases API 
 testURL = "https://covid-api.mmediagroup.fr/v1/cases?country=Germany";
@@ -17,13 +18,3 @@ fetch(testURL)
 
 
 //============FETCH OPEN WEATHER API DATA ===============
-function getOpenWeather () {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=";
-
-
-
-let newURL = "https://api.roadgoat.com/api/v2/destinations/auto_complete?q=chicago"
-fetch(newURL, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
