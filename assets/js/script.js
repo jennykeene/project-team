0628fdd5dcf1c24a640bfc88b00e234333f5c900
jennyk1989 <jennifer0keene@gmail.com>
2021-12-13 14:15:29 -0600

    html ids and script links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,24 @@
 var token = config.MY_API_TOKEN;
 var key = config.SECRET_API_KEY;
 
-//Covid Cases API 
-testURL = "https://covid-api.mmediagroup.fr/v1/cases?country=Germany";
-
-fetch(testURL)
-.then(response => {
-    if (response.ok) {
-        console.log(response);
-        response.json().then((data) => {
-            console.log(data);
-        })
-    }
-});
-
-
 
 //============FETCH OPEN WEATHER API DATA ===============
+function fetchWeather(city) {
+    const weatherURL ="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=425535dc025827a7e77aa8a4d5289d87";
+     
+    fetch(weatherURL)
+    .then(response => {
+        if (response.ok) {
+            console.log(response);
+            response.json().then((data) => {
+                console.log(data);
+            })
+        }
+    });
+
+
+}
+
+
+
+
